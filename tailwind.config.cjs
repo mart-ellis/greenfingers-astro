70eb14216724ca60e278b50d7e32993bae6b4731
Martin Ellis <martinryanellis@gmail.com>
2023-02-23 19:09:27 +0000

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        humane: ["Humane"],
+      },
+      colors: {
+        primary: "#E3FFED",
+        "custom-black": "#0F0F0F",
+        "dark-green": "#1b4529",
+      },
+    },
+  },
+  plugins: [],
+};
