2e3c032374b31b76cbf39f195ee5c362bdd4451f
Martin Ellis <martinryanellis@gmail.com>
2023-03-01 19:36:26 +0000

    Title component line height adj

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        humane: ["Humane", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: "#E3FFED",
         "custom-black": "#0F0F0F",
