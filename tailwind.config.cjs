429587fc17135bd36810cf860b23f71b38260490
Martin Ellis <martinryanellis@gmail.com>
2023-03-01 17:35:56 +0000

    Font issue on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        humane: ["Humane"],
-      },
       colors: {
         primary: "#E3FFED",
         "custom-black": "#0F0F0F",
