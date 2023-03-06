7d433bb2ecdd44d89555b651471d8e5786c3221c
Martin Ellis <martinryanellis@gmail.com>
2023-03-06 17:35:43 +0000

    Testing video bg performance

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,8 +24,8 @@ export default function Modal({ name, picture, bio, children }) {
         onClose={() => setIsOpen(false)}
         className="relative z-50"
       >
-        <div className="fixed inset-0 flex items-center justify-center bg-zinc-400 bg-opacity-60 px-22">
-          <Dialog.Panel className="w-full max-w-sm rounded bg-neutral-900 opacity-100 text-primary px-4 py-4 text-sm">
+        <div className="fixed inset-0 flex items-center justify-center bg-zinc-800 bg-opacity-60 px-22">
+          <Dialog.Panel className="w-full max-w-sm rounded bg-neutral-900 opacity-100 text-primary px-5 py-4 text-sm">
             <div className="max-w-full aspect-square overflow-hidden">
               {children}
             </div>
@@ -34,10 +34,10 @@ export default function Modal({ name, picture, bio, children }) {
             </Dialog.Title>
             <Dialog.Description></Dialog.Description>
 
-            <p className="text-xs text-justify">{bio}</p>
+            <p className="text-xs text-justify leading-5">{bio}</p>
 
             <button
-              className="w-full bg-primary mt-4 uppercase text-custom-black py-1.5 text-xs"
+              className="w-full bg-primary mt-4 uppercase text-custom-black py-2 text-xs"
               onClick={() => setIsOpen(false)}
             >
               Close
